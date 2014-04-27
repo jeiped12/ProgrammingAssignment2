4e1ee15f04f01ec2dc16077d7c997dc2a45c84dd
jeiped12 <jeiped12@users.noreply.github.com>
2014-04-27 19:33:12 +0200

    Update cachematrix.R
    I don't understand very well my code but works with example https://class.coursera.org/rprog-002/forum/thread?thread_id=696 and in the description of the assigment it says "The problem just asks you to mimic the above functions to do a Matrix Inverse Caching"

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+	setInverse <- function(solve) m <<- solve
+        getInverse <- function() m
+        list(set = set, get = get,
+	     setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverse(m)
+        m
 }
+
+
+# I don't understand very well my code but works with example
+# https://class.coursera.org/rprog-002/forum/thread?thread_id=696
